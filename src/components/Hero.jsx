3863c9115a3d3c762997075bac7d2cb5e7720bcf
Cyn Romero <cyn.jaz.romero@gmail.com>
2020-12-02 19:24:27 -0300

    Adding comments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+//dependencies
+import React from "react";
 
 const Hero = () => {
-    return ( 
-        <section className="Hero">
-            <h1 className="Hero__maintitle">Hi, I´m Cynthia Romero</h1>
-            <p className="Hero__maintitle-text">Web Developer in progress</p>
-        </section>
-     );
-}
- 
+  return (
+    <section className="Hero">
+      <h1 className="Hero__maintitle">Hi, I´m Cynthia Romero</h1>
+      <p className="Hero__maintitle-text">Web Developer in progress</p>
+    </section>
+  );
+};
+
 export default Hero;
