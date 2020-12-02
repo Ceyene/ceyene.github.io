5762d2f39bd6790a9425be8c27d14261f8e1b5b8
Cyn Romero <cyn.jaz.romero@gmail.com>
2020-12-02 19:23:21 -0300

    Importing components in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+//styles
 import "./App.css";
+//components
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Knowledge from "./components/Knowledge";
+import ProjectsContainer from "./components/ProjectsContainer";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hero />
+      <About />
+      <Knowledge />
+      <ProjectsContainer />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
