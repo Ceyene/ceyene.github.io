5432d664c7d59a1b9bfcf056a19cd0ac2e4bbc73
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-25 20:22:36 -0300

    Adding a new project and changing images source

--- src/util/projectsData.js
+++ src/util/projectsData.js
@@ -1,17 +1,29 @@
 const projectsData = [
   {
     id: "Proyect1",
+    name: "Choose Your Hero",
+    photo:
+      "https://1.bp.blogspot.com/-kT9Hsplejvo/YK2ElfjsE7I/AAAAAAAAYY4/c1NlyhEleUgbVzrFOjq3k5xsDMICCRf7ACLcBGAsYHQ/s994/chooseyourhero.jpg",
+    description: "A web app to choose your superhero team and save the world!",
+    technologies: "HTML, CSS, Vanilla JavaScript (SPA), REST.",
+    link: "https://chooseyourhero.netlify.app/",
+    repo: "https://github.com/Ceyene/ChooseYourHero",
+  },
+  {
+    id: "Proyect2",
     name: "My blog",
-    photo: "./images/frontendblog.jpg",
+    photo:
+      "https://1.bp.blogspot.com/-NYINxbzP1GM/YK2EmcoNTOI/AAAAAAAAYY8/icCbnEC1_igRASAIxZ4JBCgWIfOZ6iidACLcBGAsYHQ/s940/frontendblog.jpg",
     description: "A blog about my Front End Adventures.",
     technologies: "Gatsby, GraphQL, React, Webpack, Sass.",
     link: "https://frontenddevblog.netlify.app/",
     repo: "https://github.com/Ceyene/MyBlog",
   },
   {
-    id: "Proyect2",
+    id: "Proyect3",
     name: "Cyn Stories",
-    photo: "./images/cyncuentos.jpg",
+    photo:
+      "https://1.bp.blogspot.com/-SXnjtcf8iRs/YK2ElTes-jI/AAAAAAAAYYw/xWAn9STeHT8ncEeUHjAjIdZk_g-NHu2qQCLcBGAsYHQ/s1007/cyncuentos.jpg",
     description: "A short stories catalog.",
     technologies:
       "React (Functional Components. Hooks. React Router). Redux, Webpack, Sass.",
@@ -19,9 +31,10 @@ const projectsData = [
     repo: "https://github.com/Ceyene/CynCuentos",
   },
   {
-    id: "Proyect3",
+    id: "Proyect4",
     name: "Shopping Cart",
-    photo: "./images/mystore.jpg",
+    photo:
+      "https://1.bp.blogspot.com/-fkB042GES7E/YK2EmsomjCI/AAAAAAAAYZE/jdQ7iGqkfWYadRG2ku0bwHbh50mwYfCJACLcBGAsYHQ/s993/mystore.jpg",
     description: "A mock shopping cart.",
     technologies:
       "React (Functional Components). TypeScript, Material UI, Styled Components.",
@@ -29,9 +42,10 @@ const projectsData = [
     repo: "https://github.com/Ceyene/ShoppingCart",
   },
   {
-    id: "Proyect4",
+    id: "Proyect5",
     name: "Rewards Store",
-    photo: "./images/rewardsstore.jpg",
+    photo:
+      "https://1.bp.blogspot.com/-zylQQm5SFcU/YK2EnPkugfI/AAAAAAAAYZI/nWGWMGbJCN0J6PxR0y8QsJcbV4WADGF6QCLcBGAsYHQ/s903/rewardsstore.jpg",
     description: "A web to get some gifts in exchange for points.",
     technologies:
       "React (Functional components. Hooks. React Router). Styled Components.",
@@ -39,23 +53,15 @@ const projectsData = [
     repo: "https://github.com/Ceyene/StoreRomeroCynthia",
   },
   {
-    id: "Proyect5",
+    id: "Proyect6",
     name: "Booking System",
-    photo: "./images/descansar.jpg",
+    photo:
+      "https://1.bp.blogspot.com/-kMX_9iJCrxs/YK2ElZa0J7I/AAAAAAAAYY0/jwJgNxYSwboVhlwWwdqVGkqNj1Rs2HwNgCLcBGAsYHQ/s825/descansar.jpg",
     description:
       "A small booking system, with filters by date, rooms and price.",
     technologies: "React (Functional components. Hooks)",
     link: "https://npxdz.csb.app/",
     repo: "https://github.com/Ceyene/DescansarPuntoCom",
-  },
-  {
-    id: "Project6",
-    name: "Multi-Messenger",
-    photo: "./images/messengermultiple.jpg",
-    description: "A mock messenger service.",
-    technologies: "React (Class components)",
-    link: "https://messenger-multiple.netlify.app/",
-    repo: "https://github.com/Ceyene/MessengerMultiple",
   },
 ];
 
