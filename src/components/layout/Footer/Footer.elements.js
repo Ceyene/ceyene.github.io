4a8afef5dc66f17549c79a1c13703fce074f51bd
Ceyene <cyn.jaz.romero@gmail.com>
2021-05-28 13:20:37 -0300

    Final styling corrections

--- src/components/layout/Footer/Footer.elements.js
+++ src/components/layout/Footer/Footer.elements.js
@@ -1,8 +1,14 @@
 //dependencies
 import styled from "styled-components";
+import { StyledSmallText } from "../../styles/globalStyles";
 
 export const StyledFooter = styled.footer`
   text-align: center;
   margin-top: 0.5rem;
   font-size: 1rem;
+`;
+
+export const StyledFooterText = styled(StyledSmallText)`
+  color: #000000;
+  ${StyledSmallText}
 `;
--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,17 +1,12 @@
 //dependencies
 import React from "react";
-//assets
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faGithub, faCodepen } from "@fortawesome/free-brands-svg-icons";
-// import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 //styles
-import { StyledFooter } from "./Footer.elements";
-import { StyledSmallText } from "../../styles/globalStyles";
+import { StyledFooter, StyledFooterText } from "./Footer.elements";
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <StyledSmallText>© 2021 — Cynthia Romero</StyledSmallText>
+      <StyledFooterText>© 2021 — Cynthia Romero</StyledFooterText>
     </StyledFooter>
   );
 };
